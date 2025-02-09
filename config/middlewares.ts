a8d70ee74b81504d11d00408d061d663ac9fd57f
Marcos Luna <marcos.luna@vasscompany.com>
2025-02-09 19:42:00 +0100

    feat: Initial hispasat strapi cms commit

---
+++ config/middlewares.ts
@@ -0,0 +1,14 @@
+console.log('Registrando middleware desde: ../dist/src/middlewares/auto-populate');
+
+export default [
+  'strapi::logger',
+  'strapi::errors',
+  'strapi::security',
+  'strapi::cors',
+  'strapi::poweredBy',
+  'strapi::query',
+  'strapi::body',
+  'strapi::session',
+  'strapi::favicon',
+  'strapi::public'
+];
