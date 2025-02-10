4c3cfbded8b2ecac0377db9e7452cf65da810f98
Marcos Luna <marcos.luna@vasscompany.com>
2025-02-10 13:13:43 +0100

    feat: WIP

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,14 +1,16 @@
-console.log('Registrando middleware desde: ../dist/src/middlewares/auto-populate');
+console.log(
+  "Registrando middleware desde: ../dist/src/middlewares/auto-populate"
+);
 
 export default [
-  'strapi::logger',
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public'
+  "strapi::logger",
+  "strapi::errors",
+  "strapi::security",
+  "strapi::cors",
+  "strapi::poweredBy",
+  "strapi::query",
+  "strapi::body",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
 ];
